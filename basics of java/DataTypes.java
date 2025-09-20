50a9eb4235dde29db906a1f55864d089150aa0b4
adarsh4224 <adarshmaske2005@gmail.com>
2025-09-20 11:02:47 +0530

    1st commit

---
+++ basics of java/DataTypes.java
@@ -0,0 +1,19 @@
+public class DataTypes {
+    public static void main(String[] args){
+        int a = 5;
+        float b = 5.5f;
+        char c = 'A';
+        String s = "Hello";
+        Double d = 10.24;
+        boolean bool = true;
+
+        System.out.println(a+ " is integer");
+        System.out.println(b+ " is float");
+        System.out.println(c+ " is character");
+        System.out.println(s+ " is a String");
+        System.out.println(d+  "10.24");
+        System.out.println(bool + " is boolean");
+              
+
+    }
+}
